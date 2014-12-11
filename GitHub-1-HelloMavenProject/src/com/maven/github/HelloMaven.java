7bb390ee0c4c11dec7d484dd892e1b2840ef09fe
devGitBlit <akuanux@gmail.com>
2014-12-11 12:00:34 +0100

    Commit & Push an "Hello" Java Project on GitHub

---
+++ GitHub-1-HelloMavenProject/src/com/maven/github/HelloMaven.java
@@ -0,0 +1,10 @@
+package com.maven.github;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello Maven on GitHub with a Java Project !!");
+	}
+
+}
